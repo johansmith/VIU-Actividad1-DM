8029d3566d1a3220905f687366098acd9fb55d64
Jaime F <jfibergran@gmail.com>
2023-10-30 22:18:13 +0100

    D05 - Tachado de items (3)

--- app/src/main/java/com/masterviu/actividad1/contents/ContentTypeMedia.kt
+++ app/src/main/java/com/masterviu/actividad1/contents/ContentTypeMedia.kt
@@ -8,9 +8,5 @@ sealed class ContentTypeMedia {
     object Libros: ContentTypeMedia()
     object Deportes: ContentTypeMedia()
     object Varios: ContentTypeMedia()
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 1c482c4 (D05 - Tachado de items)
 }
